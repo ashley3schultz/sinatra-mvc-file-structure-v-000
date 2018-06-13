445acca26b94e14b9ca8c486b5606929192d7f03
Ashley Schultz <ashleyeschultz@gmail.com>
2018-06-13 17:26:29 +0000

    automatically backed up by learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,14 +1,10 @@
 class Dog
-  attr_accessor :breed
-  attr_reader :name, :age
+  attr_accessor :name, :age
+  attr_reader :breed
   def initialize(name, breed, age)
     @name = name
     @breed = breed
     @age = age
-  end
-
-  def name
-    @name
   end
 
 end
